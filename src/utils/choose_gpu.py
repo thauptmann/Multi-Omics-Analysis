1bc0bd57f958d9c90962063fa4a38abd6b7828db
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-02-17 17:31:27 +0100

    Fix bug in choose_gpu.py

--- src/utils/choose_gpu.py
+++ src/utils/choose_gpu.py
@@ -1,13 +1,12 @@
 import subprocess
-from io import StringIO
+from io import BytesIO
 import pandas as pd
 
 
 def get_free_gpu():
-    gpu_stats = subprocess.check_output(["nvidia-smi", "--format=csv", "--query-gpu=memory.used,memory.free"])
-    gpu_df = pd.read_csv(StringIO(u"".join(gpu_stats)),
-                         names=['memory.used', 'memory.free'],
+    gpu_stats = subprocess.check_output(["nvidia-smi", "--format=csv", "--query-gpu=memory.free"])
+    gpu_df = pd.read_csv(BytesIO(gpu_stats),
+                         names=['memory.free'],
                          skiprows=1)
-    gpu_df['memory.free'] = gpu_df['memory.free'].map(lambda x: x.rstrip(' [MiB]'))
-    idx = gpu_df['memory.free'].idxmax()
-    return idx
+    gpu_df['memory.free'] = gpu_df['memory.free'].map(lambda x: int(x.rstrip(' [MiB]')))
+    return gpu_df['memory.free'].idxmax()
