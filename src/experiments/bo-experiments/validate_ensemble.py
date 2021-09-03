a0f465251b52ffaa8350df66c59f4f665f84b52b
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-09-03 16:17:11 +0200

    Change weighted prediction

--- src/experiments/bo-experiments/validate_ensemble.py
+++ src/experiments/bo-experiments/validate_ensemble.py
@@ -94,11 +94,8 @@ def train_and_validate_ensemble(experiment_name, gpu_number, drug_name, extern_d
     hard_voting_auroc = roc_auc_score(y_true_list, prediction_max)
 
     # todo weighted vote
-    temperature = 2
-    y = np.exp(np.multiply(temperature , auc_list))
-    weights = y / np.sum(y)
-    weighted_predictions = np.squeeze(prediction_lists)*weights[:, np.newaxis]
-    normalised_weighted_predictions = np.sum(weighted_predictions, axis=0)
+    weighted_predictions = np.squeeze(prediction_lists)*auc_list[:, np.newaxis]
+    normalised_weighted_predictions = (np.sum(weighted_predictions, axis=0)) / np.sum(auc_list)
 
     weighted_voting_auroc = roc_auc_score(y_true_list, normalised_weighted_predictions)
 
