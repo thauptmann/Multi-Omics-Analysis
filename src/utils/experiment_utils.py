26f15e3c4864fbf524e70417ddfc0e6ce097620b
Tony Hauptman <thauptmann@users.noreply.github.com>
2021-12-22 14:20:30 +0100

    Reset results
    Delete all results of intermediate source code and experiments to restart with final code and experiment plan.

---
+++ src/utils/experiment_utils.py
@@ -0,0 +1,51 @@
+import torch
+from ax import Models
+from ax.modelbridge.generation_strategy import GenerationStrategy, GenerationStep
+
+
+def create_generation_strategy(sampling_method, sobol_iterations):
+    if sampling_method == 'gp':
+        generation_strategy = GenerationStrategy(
+            steps=[
+                GenerationStep(model=Models.SOBOL,
+                               num_trials=sobol_iterations,
+                               max_parallelism=1,
+                               model_kwargs={"seed": random_seed}),
+                GenerationStep(
+                    model=Models.BOTORCH,
+                    max_parallelism=1,
+                    num_trials=-1,
+                ),
+            ],
+            name="Sobol+GPEI"
+        )
+    elif sampling_method == 'saasbo':
+        generation_strategy = GenerationStrategy(
+            steps=[
+                GenerationStep(model=Models.SOBOL,
+                               num_trials=sobol_iterations,
+                               max_parallelism=1,
+                               model_kwargs={"seed": random_seed}),
+                GenerationStep(
+                    model=Models.FULLYBAYESIAN,
+                    num_trials=-1,
+                    max_parallelism=1,
+                    model_kwargs={
+                        "num_samples": 256,
+                        "warmup_steps": 512,
+                        "disable_progbar": True,
+                        "torch_device": torch.device('cpu'),
+                        "torch_dtype": torch.double,
+                    },
+                ),
+            ],
+            name="SAASBO"
+        )
+    else:
+        generation_strategy = GenerationStrategy(
+            steps=[
+                GenerationStep(model=Models.SOBOL, num_trials=-1),
+            ],
+            name="Sobol"
+        )
+    return generation_strategy
---
+++ src/utils/input_arguments.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def get_cmd_arguments():
+    global parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--search_iterations', default=200, type=int)
+    parser.add_argument('--sobol_iterations', default=20, type=int)
+    parser.add_argument('--experiment_name', required=True)
+    parser.add_argument('--load_checkpoint', default=False, action='store_true')
+    parser.add_argument('--combination', default=None, type=int)
+    parser.add_argument('--sampling_method', default='gp', choices=['gp', 'sobol', 'saasbo'])
+    parser.add_argument('--gpu_number', type=int)
+    parser.add_argument('--small_search_space', default=False, action='store_true')
+    parser.add_argument('--deactivate_skip_bad_iterations', default=False, action='store_true')
+    parser.add_argument('--drug', default='all', choices=['Gemcitabine_tcga', 'Gemcitabine_pdx', 'Cisplatin',
+                                                          'Docetaxel', 'Erlotinib', 'Cetuximab', 'Paclitaxel'])
+    parser.add_argument('--triplet_selector_type', default='all', choices=['all', 'hardest', 'random', 'semi_hard',
+                                                                           'none'])
+    parser.add_argument('--deactivate_elbow_method', default=True, action='store_false')
+    return parser.parse_args()
