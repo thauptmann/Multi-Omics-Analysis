7ab4edf31c2fe838baddf7427c850162a56bcdec
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-04-01 11:21:05 +0200

    Scale e data appropriate

--- src/experiments/super.felt experiments/super.felt_original.py
+++ src/experiments/super.felt experiments/super.felt_original.py
@@ -631,7 +631,7 @@ def super_felt(experiment_name, drug_name, extern_dataset_name, gpu_number, nois
             all_encoded = torch.cat((e_encoded, m_encoded, c_encoded), 1)
         else:
             with torch.no_grad():
-                e_encoded = final_E_Supervised_Encoder(torch.FloatTensor(GDSCE.to_numpy()).to(device))
+                e_encoded = final_E_Supervised_Encoder(torch.FloatTensor(final_scalerGDSC.transform(GDSCE.to_numpy())).to(device))
                 m_encoded = final_M_Supervised_Encoder(torch.FloatTensor(GDSCM.to_numpy()).to(device))
                 c_encoded = final_C_Supervised_Encoder(torch.FloatTensor(GDSCC.to_numpy()).to(device))
                 test = GDSCR
