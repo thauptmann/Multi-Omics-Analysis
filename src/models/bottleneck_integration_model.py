c13bd2b8550b064f8577d17a92e7651c45a1fb44
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-04-20 15:28:18 +0200

    Add reconstruction Mobi and Stacking

--- src/models/bottleneck_integration_model.py
+++ src/models/bottleneck_integration_model.py
@@ -43,3 +43,41 @@ class Mobi(nn.Module):
 
         stacked_logits = torch.mean(torch.concat((classified_e, classified_m, classified_c), dim=1), dim=1)
         return [stacked_logits, torch.concat((encoded_e, encoded_m, encoded_c), dim=1)]
+
+
+class MobiWithReconstruction(nn.Module):
+    def __init__(self, input_sizes, encoding_sizes, bottleneck_size, dropout):
+        super(MobiWithReconstruction, self).__init__()
+
+        self.e_encoder = Encoder(input_sizes[0], encoding_sizes[0], dropout[0])
+        self.m_encoder = Encoder(input_sizes[1], encoding_sizes[1], dropout[1])
+        self.c_encoder = Encoder(input_sizes[2], encoding_sizes[2], dropout[2])
+
+        self.e_classifier = torch.nn.Sequential(nn.Linear(encoding_sizes[0] + bottleneck_size, 1))
+        self.m_classifier = torch.nn.Sequential(nn.Linear(encoding_sizes[1] + bottleneck_size, 1))
+        self.c_classifier = torch.nn.Sequential(nn.Linear(encoding_sizes[2] + bottleneck_size, 1))
+        self.bottleneck_classifier = torch.nn.Sequential(nn.Linear(bottleneck_size, 1))
+
+        self.bottleneck_layer = nn.Linear(sum(encoding_sizes), bottleneck_size)
+
+        self.expression_decoder = nn.Linear(encoding_sizes[0], input_sizes[0])
+        self.mutation_decoder = nn.Linear(encoding_sizes[1], input_sizes[1])
+        self.cna_decoder = nn.Linear(encoding_sizes[2], input_sizes[2])
+
+    def forward(self, expression, mutation, cna):
+        encoded_e = self.e_encoder(expression)
+        encoded_m = self.m_encoder(mutation)
+        encoded_c = self.c_encoder(cna)
+        bottleneck_features = self.bottleneck_layer(torch.concat((encoded_e, encoded_m, encoded_c), dim=1))
+
+        classified_e = self.e_classifier(torch.concat((encoded_e, bottleneck_features), dim=1))
+        classified_m = self.m_classifier(torch.concat((encoded_m, bottleneck_features), dim=1))
+        classified_c = self.c_classifier(torch.concat((encoded_c, bottleneck_features), dim=1))
+
+        expression_reconstruction = self.expression_decoder(encoded_e)
+        mutation_reconstruction = self.mutation_decoder(encoded_m)
+        cna_reconstruction = self.cna_decoder(encoded_c)
+
+        stacked_logits = torch.mean(torch.concat((classified_e, classified_m, classified_c), dim=1), dim=1)
+        return [stacked_logits, torch.concat((encoded_e, encoded_m, encoded_c), dim=1),
+                expression_reconstruction, mutation_reconstruction, cna_reconstruction]
