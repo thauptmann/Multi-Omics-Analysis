25b3d6ef1d5446d9bfed8c6c8259b35028055ced
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-05-30 10:48:01 +0200

    Send target to device

--- src/utils/network_training_util.py
+++ src/utils/network_training_util.py
@@ -186,7 +186,7 @@ def train_validate_classifier(classifier_epoch, device, e_supervised_encoder,
 
 def train_classifier(classifier, classifier_epoch, train_loader, classifier_optimizer, e_supervised_encoder,
                      m_supervised_encoder, c_supervised_encoder, device):
-    bce_loss_function = torch.nn.BCEWithLogitsLoss()
+    bce_loss_function = torch.nn.BCEWithLogitsLoss().to(device)
     classifier = classifier.to(device)
     for cl_epoch in range(classifier_epoch):
         classifier.train()
