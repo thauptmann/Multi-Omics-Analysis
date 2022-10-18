655b93bf8b400933fd9ce346d53ec1dc04e511c0
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-10-18 10:34:39 +0200

    Change value type to float

--- src/utils/searchspaces.py
+++ src/utils/searchspaces.py
@@ -673,19 +673,19 @@ def create_pca_search_space():
         {
             "name": "variance_e",
             "values": parameter["variance_choices"],
-            "value_type": "int",
+            "value_type": "float",
             "type": "choice",
         },
         {
             "name": "variance_m",
             "values": parameter["variance_choices"],
-            "value_type": "int",
+            "value_type": "float",
             "type": "choice",
         },
         {
             "name": "variance_c",
             "values": parameter["variance_choices"],
-            "value_type": "int",
+            "value_type": "float",
             "type": "choice",
         },
         {
