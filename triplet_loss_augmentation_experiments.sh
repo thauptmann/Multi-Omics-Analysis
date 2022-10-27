76eb858a58654239d6102405f782c39afa2dacdf
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-10-27 17:37:52 +0200

    Change experiment name

--- triplet_loss_augmentation_experiments.sh
+++ triplet_loss_augmentation_experiments.sh
@@ -2,4 +2,4 @@ python src/experiments/early_integration/optimise_early_integration.py --experim
 python src/experiments/moli/optimise_moli.py --experiment_name moli_without_triplet_loss --deactivate_triplet_loss
 python src/experiments/moma/optimise_moma.py --experiment_name moma_with_triplet_loss --add_triplet_loss
 python src/experiments/omiEmbed/optimise_omiEmbed.py --experiment_name omiEmbed_with_triplet_loss --add_triplet_loss
-python src/experiments/super.felt/optimise_super_felt.py --experiment_name super.felt --deactivate_triplet_loss
+python src/experiments/super.felt/optimise_super_felt.py --experiment_name super_felt_without_triplet_loss --deactivate_triplet_loss
