fb1a2803d2c8d841ae02b86f4fe73aa4a1cfaddb
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-11-07 10:01:51 +0100

    Add triplet ablation study

---
+++ interpretability_experiments.sh
@@ -0,0 +1,6 @@
+python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration
+python src/experiments/moli/feature_importance_moli.py --experiment_name moli 
+python src/experiments/moma/feature_importance_moma.py --experiment_name moma 
+python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed 
+python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt 
+python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking
