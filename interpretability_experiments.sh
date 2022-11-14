b20f97c3aa2d9a191e603e6ac08f84069b609234
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-11-14 15:52:07 +0100

    Fix interpretability experiment script

--- interpretability_experiments.sh
+++ interpretability_experiments.sh
@@ -3,13 +3,13 @@ python src/experiments/moli/feature_importance_moli.py --experiment_name moli --
 python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Cetuximab
 python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Cetuximab
 python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Cetuximab
-python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking--drug Cetuximab
+python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Cetuximab
 python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Cetuximab
 
-python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --Docetaxel
-python src/experiments/moli/feature_importance_moli.py --experiment_name moli --Docetaxel
-python src/experiments/moma/feature_importance_moma.py --experiment_name moma --Docetaxel
-python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --Docetaxel
-python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --Docetaxel
-python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking--Docetaxel
-python src/experiments/pca/feature_importance_pca.py --experiment_name pca --Docetaxel
+python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --drug Docetaxel
+python src/experiments/moli/feature_importance_moli.py --experiment_name moli --drug Docetaxel
+python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Docetaxel
+python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Docetaxel
+python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Docetaxel
+python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Docetaxel
+python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Docetaxel
