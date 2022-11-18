4a04d12355e22bdb0a04a45db0809fada61f722b
Tony Hauptman <thauptmann@users.noreply.github.com>
2022-11-18 13:16:21 +0100

    Add convert ids to script

--- interpretability_experiments.sh
+++ interpretability_experiments.sh
@@ -1,15 +1,15 @@
-python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --drug Cetuximab
-python src/experiments/moli/feature_importance_moli.py --experiment_name moli --drug Cetuximab
-python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Cetuximab
-python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Cetuximab
-python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Cetuximab
-python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Cetuximab
-python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Cetuximab
+python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --drug Cetuximab --convert_ids
+python src/experiments/moli/feature_importance_moli.py --experiment_name moli --drug Cetuximab --convert_ids
+python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Cetuximab --convert_ids
+python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Cetuximab --convert_ids
+python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Cetuximab --convert_ids
+python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Cetuximab --convert_ids
+python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Cetuximab --convert_ids
 
-python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --drug Docetaxel
-python src/experiments/moli/feature_importance_moli.py --experiment_name moli --drug Docetaxel
-python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Docetaxel
-python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Docetaxel
-python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Docetaxel
-python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Docetaxel
-python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Docetaxel
+python src/experiments/early_integration/feature_importance_early_integration.py --experiment_name early_integration --drug Docetaxel --convert_ids
+python src/experiments/moli/feature_importance_moli.py --experiment_name moli --drug Docetaxel --convert_ids
+python src/experiments/moma/feature_importance_moma.py --experiment_name moma --drug Docetaxel --convert_ids
+python src/experiments/omiEmbed/feature_importance_omiEmbed.py --experiment_name omiEmbed --drug Docetaxel --convert_ids
+python src/experiments/super.felt/feature_importance_super_felt.py --experiment_name super_felt --drug Docetaxel --convert_ids
+python src/experiments/stacking/feature_importance_stacking.py --experiment_name stacking --drug Docetaxel --convert_ids
+python src/experiments/pca/feature_importance_pca.py --experiment_name pca --drug Docetaxel --convert_ids
